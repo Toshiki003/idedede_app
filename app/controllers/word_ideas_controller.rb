59107e17954d4ce4757e3c86495fbc1e51a9fe32
関目智史 <zou3road@gmail.com>
2023-06-10 00:46:15 +0900

    word_ideasコントローラにnewとcreateアクションを記述

--- app/controllers/word_ideas_controller.rb
+++ app/controllers/word_ideas_controller.rb
@@ -1,4 +1,15 @@
 class WordIdeasController < ApplicationController
   def index
+    @word_ideas = WordIdea.all
+    session[:random_word] = Word.sample.id
+    @word = Word.find(session[:random_id])
+  end
+
+  def create
+    @word = Word.find(session[:random_id])
+    @word_idea = WordIdea.create(word: @word, idea: Idea.last)
+    if @word_idea.save
+      redirect_to word_ideas_path
+    end
   end
 end
