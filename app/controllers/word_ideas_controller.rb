1e10b4be7a8eed78be7dc7dc80d9407d0518fd54
関目智史 <zou3road@gmail.com>
2023-06-10 00:59:20 +0900

    データベースからランダムでワードを取得するsampleメソッドをモデルに自作

--- app/controllers/word_ideas_controller.rb
+++ app/controllers/word_ideas_controller.rb
@@ -2,11 +2,11 @@ class WordIdeasController < ApplicationController
   def index
     @word_ideas = WordIdea.all
     session[:random_word] = Word.sample.id
-    @word = Word.find(session[:random_id])
+    @word = Word.find(session[:random_word])
   end
 
   def create
-    @word = Word.find(session[:random_id])
+    @word = Word.find(session[:random_word])
     @word_idea = WordIdea.create(word: @word, idea: Idea.last)
     if @word_idea.save
       redirect_to word_ideas_path
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,9 @@
 class Word < ApplicationRecord
   has_many :word_ideas
+
+  # データベースからランダムにワードを取得するためのメソッドを自作する
+  def self.sample 
+    self.find(rand(1..Word.count))
+    p self.find(rand(1..Word.count))
+  end
 end
